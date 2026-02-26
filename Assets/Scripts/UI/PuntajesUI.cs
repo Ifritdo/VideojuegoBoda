25215426fc269651c2e7ecc23c5c0130e84a4567
Ifritdo <JulianFirpo316@hotmail.com>
2026-02-25 23:15:14 -0300

    Commit Puntajes
    Se inicio la escena para la representacion de los scores por el momento todo visual nada funcional

--- Assets/Scripts/UI/PuntajesUI.cs
+++ Assets/Scripts/UI/PuntajesUI.cs
@@ -1,34 +1,111 @@
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class PuntajesUI : MonoBehaviour
 {
-    public InputField nameInput;
-    public Text scoreText;
-    public Text rankingText;
+    [Header("Input")]
+    [SerializeField] private TMP_InputField nameInput;
+
+    [Header("Podio")]
+    [SerializeField] private TextMeshProUGUI firstName;
+    [SerializeField] private TextMeshProUGUI firstScore;
+    [SerializeField] private TextMeshProUGUI secondName;
+    [SerializeField] private TextMeshProUGUI secondScore;
+    [SerializeField] private TextMeshProUGUI thirdName;
+    [SerializeField] private TextMeshProUGUI thirdScore;
+
+    [Header("Columnas")]
+    [SerializeField] private Transform column1;
+    [SerializeField] private Transform column2;
+    [SerializeField] private Transform column3;
+
+    [SerializeField] private GameObject rankingItemPrefab;
+    [SerializeField] private int maxDisplay = 18;
 
     private void Start()
     {
-        scoreText.text = "Puntaje: " + ScoreManager.Instance.GetFinalScore();
         DisplayRanking();
     }
 
     public void ConfirmScore()
     {
+        if (string.IsNullOrWhiteSpace(nameInput.text))
+            return;
+
         ScoreManager.Instance.SaveScore(nameInput.text);
         DisplayRanking();
     }
 
     void DisplayRanking()
     {
-        rankingText.text = "";
+        var scores = ScoreManager.Instance.scores;
 
-        for (int i = 0; i < ScoreManager.Instance.scores.Count; i++)
+        // ===== PODIO =====
+        if (scores.Count > 0)
         {
-            var s = ScoreManager.Instance.scores[i];
-            rankingText.text += $"{i + 1}. {s.name} - {s.points}\n";
+            firstName.text = scores[0].name;
+            firstScore.text = scores[0].points.ToString();
         }
+
+        if (scores.Count > 1)
+        {
+            secondName.text = scores[1].name;
+            secondScore.text = scores[1].points.ToString();
+        }
+
+        if (scores.Count > 2)
+        {
+            thirdName.text = scores[2].name;
+            thirdScore.text = scores[2].points.ToString();
+        }
+
+        // ===== COLUMNAS =====
+        ClearColumn(column1);
+        ClearColumn(column2);
+        ClearColumn(column3);
+
+        int listStart = 3;
+
+        if (scores.Count <= listStart)
+            return;
+
+        int totalListItems = Mathf.Min(scores.Count, maxDisplay) - listStart;
+        totalListItems = Mathf.Max(0, totalListItems);
+
+        int rows = Mathf.CeilToInt(totalListItems / 3f);
+
+        for (int i = 0; i < totalListItems; i++)
+        {
+            int scoreIndex = listStart + i;
+
+            GameObject item = Instantiate(rankingItemPrefab);
+
+            item.transform.Find("PositionText")
+                .GetComponent<TextMeshProUGUI>().text =
+                (scoreIndex + 1) + "°";
+
+            item.transform.Find("NameText")
+                .GetComponent<TextMeshProUGUI>().text =
+                scores[scoreIndex].name;
+
+            item.transform.Find("ScoreText")
+                .GetComponent<TextMeshProUGUI>().text =
+                scores[scoreIndex].points.ToString();
+
+            if (i < rows)
+                item.transform.SetParent(column1, false);
+            else if (i < rows * 2)
+                item.transform.SetParent(column2, false);
+            else
+                item.transform.SetParent(column3, false);
+        }
+    }
+
+    void ClearColumn(Transform column)
+    {
+        foreach (Transform child in column)
+            Destroy(child.gameObject);
     }
 
     public void BackToMenu()
